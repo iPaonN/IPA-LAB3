edb91fd6e3601783d6cb5c68e34278db7b344f4a
NetAcad DEVASC <changeme@example.com>
2025-07-27 14:49:52 +0000

    fix things

--- paramikolab/get_R0_config.py
+++ paramikolab/get_R0_config.py
@@ -2,35 +2,35 @@ import time
 import paramiko
 
 username = 'admin'
-target_ip = "172.31.21.4"
+target_ip = ["172.31.21.1", "172.31.21.2", "172.31.21.3", "172.31.21.4", "172.31.21.5"]
 key_path = '/home/devasc/.ssh/id_rsa'
 
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+for ip in target_ip:
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-print(f"Connecting to {target_ip} as {username}...")
+    print(f"Connecting to {ip} as {username}...")
 
-client.connect(
-    hostname=target_ip,
-    username=username,
-    key_filename=key_path,
-    look_for_keys=True,
-    allow_agent=False,
-    timeout=20,
-    disabled_algorithms={'pubkeys': ['rsa-sha2-256', 'rsa-sha2-512']}
-)
+    client.connect(
+        hostname=ip,
+        username=username,
+        key_filename=key_path,
+        # look_for_keys=True,
+        allow_agent=False,
+        timeout=20,
+        disabled_algorithms={'pubkeys': ['rsa-sha2-256', 'rsa-sha2-512']}
+    )
+    with client.invoke_shell() as ssh:
+        print("Interactive shell established.")
 
-with client.invoke_shell() as ssh:
-    print("Interactive shell established.")
+        ssh.send("terminal length 0\n")
+        time.sleep(1)
+        output = ssh.recv(1000).decode('ascii')
+        print(output)
 
-    ssh.send("terminal length 0\n")
-    time.sleep(1)
-    output = ssh.recv(1000).decode('ascii')
-    print(output)
+        ssh.send("show run\n")
+        time.sleep(5)
+        output = ssh.recv(5000).decode('ascii')
+        print(output)
 
-    ssh.send("show run\n")
-    time.sleep(5)
-    output = ssh.recv(5000).decode('ascii')
-    print(output)
-
-    client.close()
+        client.close()
